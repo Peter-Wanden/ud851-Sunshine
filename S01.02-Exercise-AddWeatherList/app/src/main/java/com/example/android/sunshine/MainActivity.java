979ccee520de120d5b4c47dfe23cf458309ba82a
Peter Wanden <peter.wanden@gmail.com>
2017-11-07 10:37:29 +0000

    fix: Completed TODO's As requested

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,35 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // COMPLETED (1) Create a field to store the weather display TextView
+    TextView weatherDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
+        weatherDisplay = (TextView)findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // COMPLETED (3) Create an array of Strings that contain fake weather data
+        String [] weatherData = new String[] {
+                "London: 18Deg and Sunny",
+                "Barbados: -5Deg and Snow",
+                "Stockholm: 27Deg with light showers",
+                "Chicago: -30Deg with heavy snow drifts",
+                "UAE: -2 Heavy snow warning",
+                "Monaco: +22 Blue skies Nice!",
+                "St Petersberg: +18 Positively balmy"
+        };
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // COMPLETED (4) Append each String from the fake weather data array to the TextView
+        for (String weather : weatherData) {
+            weatherDisplay.append(weather + "\n\n\n");
+        }
     }
 }

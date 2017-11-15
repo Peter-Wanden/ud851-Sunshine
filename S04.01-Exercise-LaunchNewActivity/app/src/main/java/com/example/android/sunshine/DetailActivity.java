448ac1f66d16f0803614b6408985888fb29dc0e5
Peter Wanden <peter.wanden@gmail.com>
2017-11-15 16:32:54 +0000

    add: S04.01 Complete TODO's Lesson 5.23 Add a New Activity

---
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * com.example.android.sunshine
+ * Created by Peter Wanden on 15/11/2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}

7adc4fbc723a1ef13fda9356fd8f4e0b49b7de00
Peter Wanden <peter.wanden@gmail.com>
2017-11-15 17:50:23 +0000

    add: S04.03 Complete TODO's Lesson 5.25 Add Sharing

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,11 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -28,7 +32,40 @@ public class DetailActivity extends AppCompatActivity {
             }
         }
     }
-
-    // TODO (3) Create a menu with an item with id of action_share
     // TODO (4) Display the menu and implement the forecast sharing functionality
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.detail, menu);
+        /* Return true so that the menu is displayed in the Toolbar */
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            ShareText(mForecast);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    // shareText method that accepts a String as a parameter
+    public void ShareText(String text) { // From T04b.03
+
+        // COMPLETED (2) Create a String variable called mimeType and set it to "text/plain"
+        String mimeType = "text/plain";
+
+        // COMPLETED (3) Create a title for the chooser window that will pop up
+        String title = "Learning How to Share";
+
+        // COMPLETED (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+        ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setChooserTitle(title)
+                .setText(text)
+                .startChooser();
+    }
 }

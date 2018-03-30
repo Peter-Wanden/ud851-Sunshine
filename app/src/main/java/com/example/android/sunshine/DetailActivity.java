284fb8376601fec60c81ebf32794bbe03100df97
Peter.Wanden <peter.wanden@gmail.com>
2018-03-30 16:27:56 +0100

    Update gradle and dependencies

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -101,7 +101,6 @@ public class DetailActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         mDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
         mUri = getIntent().getData();
